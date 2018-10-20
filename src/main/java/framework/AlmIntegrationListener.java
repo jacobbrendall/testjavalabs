09d036a31b7931779ff3025019103ac964ec3c7f
gx <fairwinds0099@gmail.com>
2018-10-19 21:29:53 -0500

    including AlmINtegration

---
+++ src/main/java/framework/AlmIntegrationListener.java
@@ -0,0 +1,15 @@
+package framework;
+
+import org.testng.ITestResult;
+import org.testng.TestListenerAdapter;
+
+public class AlmIntegrationListener extends TestListenerAdapter {
+
+    public AlmIntegrationListener() {
+    }
+
+    public void onTestSuccess(ITestResult tr) {
+        
+    }
+
+}
