75d9120123127fb06690982a6f84278e39bc4b86
ike <ike7995@gmail.com>
2018-11-01 23:37:25 -0500

    Merge branch 'feature' of https://github.com/fairwinds0099/testjavalabs into Repo6_Branch6
    # Conflicts:
    #	src/main/java/utils/DbUtils/Dao.java

--- src/main/java/tests/DbTest.java
+++ src/main/java/tests/DbTest.java
@@ -4,26 +4,40 @@ package tests;
 import org.junit.Test;
 import utils.DbUtils.Dao;
 
+import javax.xml.crypto.URIReferenceException;
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Properties;
 
 // TODO updated
 public class DbTest extends Dao {
-    String queryRead;
-    @Test
+    Properties prop = new Properties();
 
-    public void myFirstTest() throws Exception{
-        String queryDelete;
-    }
+        prop.load(new FileInputStream("config.properties");
+    String user = prop.getProperty("dbUri");
+    String pass = prop.getProperty("userName");
+    String port = prop.getProperty("password");
+
+    final String queryCreate = "INSERT INTO ADDRESS (ID, STREETNAME, CITY, ZIPCODE, STATE)" +
+            "VALUES(7, '311 True St', 'Richmond', '54544', 'VA')";
+    final String queryRead = "SELECT * FROM ADDRESS ";
+    final String queryUpdate = "UPDATE ADDRESS SET CITY = 'Miami', ZIPCODE='22222' WHERE ID='7'";
+    final String queryDelete = "DELETE FROM ADDRESS WHERE ID=7";
     @Test
-    public void testToReadFromSql() throws Exception{
-        List<String> retrievedCities = getAllRecordsForColumn("CITY");
-        System.out.println(retrievedCities.toString());
-        }
-    @Test
-    public void myTest() throws SQLException {
-        List<String> retrievedStates = getAllRecordsForColumn(
-         "LASTNAME");
-        System.out.println(retrievedStates.toString());
+    public void myTest(URIReferenceException ex) throws SQLException {
+        Dao dao = new Dao();
+      //  List<String> retrievedStates = getAllRecordsForColumn(prop.getProperty.dbUri, userName, password, queryRead,"STATE" );
+
+     //   System.out.println(retrievedStates.toString());
+     //   catch (IOException ex) {
+     //       ex.printStackTrace();
+//        } finally {
+//            if (input != null) {
+//                try {
+//                    input.close();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+                }
     }
-}
+
--- src/main/java/utils/DbUtils/Dao.java
+++ src/main/java/utils/DbUtils/Dao.java
@@ -10,18 +10,7 @@ import java.util.List;
 
 public class Dao {
 
-    String dbUri = "jdbc:oracle:thin:@//florida.csr9s71rdbw2.us-east-1.rds.amazonaws.com:1521/FLORIDA";
-    String userName = "florida";
-    String password = "miamiflorida";
-
-    // We need to make these variables final so they cannot be changed by someone else
-    final String queryCreate = "INSERT INTO ADDRESS (ID, STREETNAME, CITY, ZIPCODE, STATE)" +
-            "VALUES(7, '311 True St', 'Richmond', '54544', 'VA')";
-    final String queryRead = "SELECT * FROM ADDRESS ";
-    final String queryUpdate = "UPDATE ADDRESS SET CITY = 'Miami', ZIPCODE='22222' WHERE ID='7'";
-    final String queryDelete = "DELETE FROM ADDRESS WHERE ID=7";
-
-    public ResultSet executeQuery(String dbUri, String userName, String password, String sqlQuery) {
+    private ResultSet executeQuery(String dbUri, String userName, String password, String sqlQuery) {
         ResultSet resultSet = null;
         try {
             Statement statement = getDbConnection(dbUri, userName, password).prepareStatement(sqlQuery);
@@ -31,26 +20,16 @@ public class Dao {
         }
         return resultSet;
     }
-    // TODO Consider modifiying access modifier here
-    //TODO try to understand why we have extracted the getDbCOnnection method
-    public Connection getDbConnection(String dbUri, String userName, String password) {
+    private Connection getDbConnection(String dbUri, String userName, String password) throws SQLException {
         Connection conn = null;
-        try {
+        //try {
             conn = DriverManager.getConnection(dbUri, userName, password);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+       // } catch (SQLException e) {
+         //   e.printStackTrace();
+       // }
         return conn;
     }
-
-    //TODO for collaborators
-    // Add a method to get all records as a List for the given column
-    // Hint: this method will return List<String>
-    // ResultSet from an ORACLE database returns first line the name of the columns
-    // so you need to perform a rs.next() before retrieveing column content.
-    // In Test CLass print all records
-    // You will use queryRead
-    public List<String> getAllRecordsForColumn(String columnName) throws SQLException {
+    public List<String> getAllRecordsForColumn(String dbUri, String userName, String password, String queryRead, String columnName) throws SQLException {
         List<String> listOfColumns = new ArrayList<String>();
 
        ResultSet rs = executeQuery(dbUri, userName, password, queryRead);
