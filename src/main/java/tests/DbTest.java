91b81a94f373bc91e1c46692c4f3db4fcf68998c
ike <ike7995@gmail.com>
2018-11-03 21:32:19 -0500

    Merge branch 'feature' of https://github.com/fairwinds0099/testjavalabs into Repo6_Branch6
    # Conflicts:
    #	src/main/java/utils/DbUtils/Dao.java

--- src/main/java/tests/DbTest.java
+++ src/main/java/tests/DbTest.java
@@ -1,29 +1,41 @@
 package tests;
 
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 import utils.DbUtils.Dao;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Properties;
 
 // TODO updated
 public class DbTest extends Dao {
     String queryRead;
-    @Test
+    static String dbUri;
+    static String userName;
+    static String password;
+    static Properties prop = new Properties();
 
-    public void myFirstTest() throws Exception{
-        String queryDelete;
+    @BeforeClass
+    public static void init() throws IOException {
+        prop.load(new FileInputStream("config.properties"));
+        dbUri = prop.getProperty("dbUri");
+        userName = prop.getProperty("userName");
+        password = prop.getProperty("password");
     }
     @Test
-    public void testToReadFromSql() throws Exception{
-        List<String> retrievedCities = getAllRecordsForColumn("CITY");
-        System.out.println(retrievedCities.toString());
+    public void testToReadFromSql() throws Exception {
+        List<String> retrievedCities = getAllRecordsForColumn(dbUri, userName, password, queryRead, "STATE");
+            System.out.println(retrievedCities.toString());
         }
+
     @Test
-    public void myTest() throws SQLException {
-        List<String> retrievedStates = getAllRecordsForColumn(
-         "LASTNAME");
-        System.out.println(retrievedStates.toString());
-    }
-}
+    public void myTest () throws SQLException {
+        List<String> retrievedStates = getAllRecordsForColumn(dbUri, userName, password, queryRead, "STATE");
+            System.out.println(retrievedStates.toString());
+            }
+        }
