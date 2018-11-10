dfdfe5a6049ec57f314bb3bf67b266c661b6b2d8
gx <fairwinds0099@gmail.com>
2018-11-10 16:54:51 -0600

    house test added

--- src/main/java/domain/pages/HomePage.java
+++ src/main/java/domain/pages/HomePage.java
@@ -1,4 +1,37 @@
 package domain.pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class HomePage {
+
+    WebDriver driver;
+
+    @FindBy(css = "#block-mainnavigation > ul > li:nth-child(1) > a")
+    private WebElement representativesLink;
+
+    @FindBy(xpath = "//input[@id='header-find-rep-zip-code']")
+    private WebElement homePageZipCodeInput;
+
+    @FindBy(css = "#block-mainnavigation > ul > li:nth-child(2) > a")
+    private WebElement leadershipLink;
+
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void clickRepresentativesLink() {
+        representativesLink.click();
+    }
+
+    public boolean isHomePageZipCodeInputPresent() {
+        return homePageZipCodeInput.isDisplayed();
+    }
+
+    public void clickLeaderShipLink() {
+        leadershipLink.click();
+    }
 }
